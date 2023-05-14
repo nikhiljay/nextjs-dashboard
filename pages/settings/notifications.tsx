d673d160d96837ccd9b6b954b5a75890891b1274
Nikhil D'Souza <nikhiljay7@gmail.com>
2023-05-13 23:16:29 -0700

    Changed notification content

--- pages/settings/notifications.tsx
+++ pages/settings/notifications.tsx
@@ -44,272 +44,130 @@ export default function Notifications() {
             </p>
           </div>
 
-          <form className="md:col-span-2">
-            <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
-              <div className="col-span-full flex items-center gap-x-8">
-                <img
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                  className="h-24 w-24 flex-none rounded-lg bg-gray-200 object-cover"
-                />
-                <div>
-                  <button
-                    type="button"
-                    className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          <div className="max-w-2xl space-y-10 md:col-span-2">
+            <fieldset>
+              <legend className="text-sm font-semibold leading-6 text-gray-900">
+                By Email
+              </legend>
+              <div className="mt-6 space-y-6">
+                <div className="relative flex gap-x-3">
+                  <div className="flex h-6 items-center">
+                    <input
+                      id="comments"
+                      name="comments"
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                    />
+                  </div>
+                  <div className="text-sm leading-6">
+                    <label
+                      htmlFor="comments"
+                      className="font-medium text-gray-900"
+                    >
+                      Comments
+                    </label>
+                    <p className="text-gray-500">
+                      Get notified when someones posts a comment on a posting.
+                    </p>
+                  </div>
+                </div>
+                <div className="relative flex gap-x-3">
+                  <div className="flex h-6 items-center">
+                    <input
+                      id="candidates"
+                      name="candidates"
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                    />
+                  </div>
+                  <div className="text-sm leading-6">
+                    <label
+                      htmlFor="candidates"
+                      className="font-medium text-gray-900"
+                    >
+                      Candidates
+                    </label>
+                    <p className="text-gray-500">
+                      Get notified when a candidate applies for a job.
+                    </p>
+                  </div>
+                </div>
+                <div className="relative flex gap-x-3">
+                  <div className="flex h-6 items-center">
+                    <input
+                      id="offers"
+                      name="offers"
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                    />
+                  </div>
+                  <div className="text-sm leading-6">
+                    <label
+                      htmlFor="offers"
+                      className="font-medium text-gray-900"
+                    >
+                      Offers
+                    </label>
+                    <p className="text-gray-500">
+                      Get notified when a candidate accepts or rejects an offer.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </fieldset>
+            <fieldset>
+              <legend className="text-sm font-semibold leading-6 text-gray-900">
+                Push Notifications
+              </legend>
+              <p className="mt-1 text-sm leading-6 text-gray-600">
+                These are delivered via SMS to your mobile phone.
+              </p>
+              <div className="mt-6 space-y-6">
+                <div className="flex items-center gap-x-3">
+                  <input
+                    id="push-everything"
+                    name="push-notifications"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+                  <label
+                    htmlFor="push-everything"
+                    className="block text-sm font-medium leading-6 text-gray-900"
                   >
-                    Change Avatar
-                  </button>
-                  <p className="mt-2 text-xs leading-5 text-gray-600">
-                    JPG, GIF or PNG. 1MB max.
-                  </p>
+                    Everything
+                  </label>
                 </div>
-              </div>
-
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="first-name"
-                  className="block text-sm font-medium leading-6 text-gray-700"
-                >
-                  First name
-                </label>
-                <div className="mt-2">
+                <div className="flex items-center gap-x-3">
                   <input
-                    type="text"
-                    name="first-name"
-                    id="first-name"
-                    autoComplete="given-name"
-                    className="block w-full rounded-md border-gray-300 shadow-sm py-1.5 text-gray-700 focus:ring-1 focus:ring-vitalize-secondary sm:text-sm sm:leading-6"
+                    id="push-email"
+                    name="push-notifications"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                   />
-                </div>
-              </div>
-
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="last-name"
-                  className="block text-sm font-medium leading-6 text-gray-700"
-                >
-                  Last name
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="last-name"
-                    id="last-name"
-                    autoComplete="family-name"
-                    className="block w-full rounded-md border-gray-300 shadow-sm py-1.5 text-gray-700 focus:ring-1 focus:ring-vitalize-secondary sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div className="col-span-full">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-700"
-                >
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    className="block w-full rounded-md border-gray-300 shadow-sm py-1.5 text-gray-700 focus:ring-1 focus:ring-vitalize-secondary sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div className="col-span-full">
-                <label
-                  htmlFor="username"
-                  className="block text-sm font-medium leading-6 text-gray-700"
-                >
-                  Username
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="username"
-                    name="username"
-                    type="text"
-                    autoComplete="username"
-                    className="block w-full rounded-md border-gray-300 shadow-sm py-1.5 text-gray-700 focus:ring-1 focus:ring-vitalize-secondary sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div className="col-span-full">
-                <label
-                  htmlFor="timezone"
-                  className="block text-sm font-medium leading-6 text-gray-700"
-                >
-                  Timezone
-                </label>
-                <div className="mt-2">
-                  <select
-                    id="timezone"
-                    name="timezone"
-                    className="block w-full rounded-md border-gray-300 shadow-sm py-1.5 text-gray-700 focus:ring-1 focus:ring-vitalize-secondary sm:text-sm sm:leading-6"
+                  <label
+                    htmlFor="push-email"
+                    className="block text-sm font-medium leading-6 text-gray-900"
                   >
-                    <option>Pacific Standard Time</option>
-                    <option>Eastern Standard Time</option>
-                    <option>Greenwich Mean Time</option>
-                  </select>
+                    Same as email
+                  </label>
+                </div>
+                <div className="flex items-center gap-x-3">
+                  <input
+                    id="push-nothing"
+                    name="push-notifications"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+                  <label
+                    htmlFor="push-nothing"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    No push notifications
+                  </label>
                 </div>
               </div>
-            </div>
-
-            <div className="mt-8 flex">
-              <button
-                type="submit"
-                className="rounded-md bg-vitalize-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-vitalize-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-vitalize-secondary"
-              >
-                Save
-              </button>
-            </div>
-          </form>
-        </div>
-
-        <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
-          <div>
-            <h2 className="text-base font-semibold leading-7 text-gray-700">
-              Change password
-            </h2>
-            <p className="mt-1 text-sm leading-6 text-gray-600">
-              Update your password associated with your account.
-            </p>
+            </fieldset>
           </div>
-
-          <form className="md:col-span-2">
-            <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
-              <div className="col-span-full">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-700"
-                >
-                  Password
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    className="block w-full rounded-md border-gray-300 shadow-sm py-1.5 text-gray-700 focus:ring-1 focus:ring-vitalize-secondary sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div className="col-span-full">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-700"
-                >
-                  New password
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="new-password"
-                    name="new_password"
-                    type="password"
-                    autoComplete="new-password"
-                    className="block w-full rounded-md border-gray-300 shadow-sm py-1.5 text-gray-700 focus:ring-1 focus:ring-vitalize-secondary sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div className="col-span-full">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-700"
-                >
-                  Confirm password
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="confirm-password"
-                    name="confirm_password"
-                    type="password"
-                    autoComplete="new-password"
-                    className="block w-full rounded-md border-gray-300 shadow-sm py-1.5 text-gray-700 focus:ring-1 focus:ring-vitalize-secondary sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="mt-8 flex">
-              <button
-                type="submit"
-                className="rounded-md bg-vitalize-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-vitalize-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-vitalize-secondary"
-              >
-                Save
-              </button>
-            </div>
-          </form>
-        </div>
-
-        <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
-          <div>
-            <h2 className="text-base font-semibold leading-7 text-gray-700">
-              Log out other sessions
-            </h2>
-            <p className="mt-1 text-sm leading-6 text-gray-500">
-              Please enter your password to confirm you would like to log out of
-              your other sessions across all of your devices.
-            </p>
-          </div>
-
-          <form className="md:col-span-2">
-            <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
-              <div className="col-span-full">
-                <label
-                  htmlFor="logout-password"
-                  className="block text-sm font-medium leading-6 text-gray-700"
-                >
-                  Your password
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="logout-password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    className="block w-full rounded-md border-gray-300 shadow-sm py-1.5 text-gray-700 focus:ring-1 focus:ring-vitalize-secondary sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="mt-8 flex">
-              <button
-                type="submit"
-                className="rounded-md bg-vitalize-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-vitalize-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-vitalize-secondary"
-              >
-                Log out other sessions
-              </button>
-            </div>
-          </form>
-        </div>
-
-        <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
-          <div>
-            <h2 className="text-base font-semibold leading-7 text-gray-700">
-              Delete account
-            </h2>
-            <p className="mt-1 text-sm leading-6 text-gray-500">
-              No longer want to use our service? You can delete your account
-              here. This action is not reversible. All information related to
-              this account will be deleted permanently.
-            </p>
-          </div>
-
-          <form className="flex items-start md:col-span-2">
-            <button
-              type="submit"
-              className="rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-400"
-            >
-              Yes, delete my account
-            </button>
-          </form>
         </div>
       </div>
     </main>
