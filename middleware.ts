a8ec6221d29bbebfcd4f823173a37a47268ad168
Lode Sai Charan <91114649+charann29@users.noreply.github.com>
2023-10-23 23:42:56 +0530

    Updated middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,31 +3,27 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
+  // Allow access to login and signup pages without authentication
   if (["/login", "/signup"].includes(req.nextUrl.pathname)) {
     return NextResponse.next();
   }
-  // We need to create a response and hand it to the supabase client to be able to modify the response headers.
-  const res = NextResponse.next();
-  // Create authenticated Supabase Client.
-  const supabase = createMiddlewareSupabaseClient({ req, res });
-  // Check if we have a session
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
 
-  // Check auth condition
+  // Create authenticated Supabase Client and retrieve the session
+  const { res } = createMiddlewareSupabaseClient({ req });
+  const { data: { session } } = await res.auth.getSession();
+
   if (session?.user) {
-    // Authentication successful, forward request to protected route.
-    return res;
+    // Authentication successful, continue to the protected route
+    return NextResponse.next();
   }
 
-  // Auth condition not met, redirect to home page.
-  const redirectUrl = req.nextUrl.clone();
-  redirectUrl.pathname = "/login";
-  redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname);
+  // Redirect to the login page with the original requested URL as a query parameter
+  const redirectUrl = new URL("/login", req.nextUrl.origin);
+  redirectUrl.searchParams.set("redirectedFrom", req.nextUrl.pathname);
   return NextResponse.redirect(redirectUrl);
 }
 
 export const config = {
-  matcher: "/((?!_next|api/auth).*)(.+)",
+  // Match all routes except those starting with _next or api/auth
+  matcher: "/((?!_next|api/auth).*)",
 };
